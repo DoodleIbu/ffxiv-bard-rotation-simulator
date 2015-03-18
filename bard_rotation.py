b7d8eb462925cb3ce2203676352e401f54af2611
kernelthree <j.v.julien@gmail.com>
2015-03-18 03:43:59 -0400

    Updated simulator, still a work in progress

--- bard_rotation.py
+++ bard_rotation.py
@@ -15,6 +15,15 @@ GCD_DELAY = 1.0
 class Effect:
     name = ""
     duration = 0
+    has_snapshot = False
+
+    @staticmethod
+    def tick(origin, target):
+        pass
+
+    @staticmethod
+    def damage_modifier():
+        return {}
 
 class SilenceEffect(Effect):
     name = "Silence"
@@ -24,17 +33,41 @@ class InternalReleaseEffect(Effect):
     name = "Internal Release"
     duration = 15
 
+    @staticmethod
+    def damage_modifier():
+        return {
+            "critical_hit_rate_add": 0.10
+        }
+
 class BloodForBloodEffect(Effect):
     name = "Blood for Blood"
     duration = 15
+
+    @staticmethod
+    def damage_modifier():
+        return {
+            "potency_multiply": 1.10
+        }
 
 class RagingStrikesEffect(Effect):
     name = "Raging Strikes"
     duration = 20
 
+    @staticmethod
+    def damage_modifier():
+        return {
+            "potency_multiply": 1.20
+        }
+
 class HawksEyeEffect(Effect):
     name = "Hawk's Eye"
     duration = 20
+
+    @staticmethod
+    def damage_modifier():
+        return {
+            "potency_multiply": 1.13 # about
+        }
 
 class BarrageEffect(Effect):
     name = "Barrage"
@@ -44,6 +77,12 @@ class StraightShotEffect(Effect):
     name = "Straight Shot"
     duration = 20
 
+    @staticmethod
+    def damage_modifier():
+        return {
+            "critical_hit_rate_add": 0.10
+        }
+
 class StraighterShotEffect(Effect):
     name = "Straighter Shot"
     duration = 10
@@ -52,17 +91,39 @@ class XPotionOfDexterityEffect(Effect):
     name = "Medicated"
     duration = 15
 
+    @staticmethod
+    def damage_modifier():
+        return {
+            "potency_multiply": 1.11 # about
+        }
+
 class FlamingArrowEffect(Effect):
     name = "Flaming Arrow"
     duration = 30
+
+    @staticmethod
+    def tick(origin, target):
+        target.add_damage(30, snapshot)
 
 class VenomousBiteEffect(Effect):
     name = "Venomous Bite"
     duration = 18
 
+    @staticmethod
+    def tick(origin, target):
+        result = target.add_damage(35, snapshot)
+        if result["critical_hit"] is True and random.random() < 0.5:
+            origin.set_cooldown(Bloodletter, 0)
+
 class WindbiteEffect(Effect):
     name = "Windbite"
     duration = 18
+
+    @staticmethod
+    def tick(origin, target, snapshot):
+        result = target.add_damage(45, snapshot)
+        if result["critical_hit"] is True and random.random() < 0.5:
+            origin.set_cooldown(Bloodletter, 0)
 
 # Data on skills
 class Skill:
@@ -71,7 +132,8 @@ class Skill:
     cooldown = 0
     tp_cost = 0
 
-    def use(self, player, target):
+    @staticmethod
+    def use(origin, target):
         pass
 
 class InternalRelease(Skill):
@@ -79,132 +141,162 @@ class InternalRelease(Skill):
     animation_lock = SHORT_DELAY
     cooldown = 60
 
-    def use(self, player, target):
-        player.add_effect(InternalReleaseEffect)
+    @staticmethod
+    def use(origin, target):
+        origin.add_effect(InternalReleaseEffect)
 
 class BloodForBlood(Skill):
     name = "Blood for Blood"
     animation_lock = SHORT_DELAY
     cooldown = 80
 
-    def use(self, player, target):
-        player.add_effect(BloodForBloodEffect)
+    @staticmethod
+    def use(origin, target):
+        origin.add_effect(BloodForBloodEffect)
 
 class RagingStrikes(Skill):
     name = "Raging Strikes"
     animation_lock = SHORT_DELAY
     cooldown = 120
 
-    def use(self, player, target):
-        player.add_effect(RagingStrikesEffect)
+    @staticmethod
+    def use(origin, target):
+        origin.add_effect(RagingStrikesEffect)
 
 class HawksEye(Skill):
     name = "Hawk's Eye"
     animation_lock = SHORT_DELAY
     cooldown = 90
 
-    def use(self, player, target):
-        player.add_effect(HawksEyeEffect)
+    @staticmethod
+    def use(origin, target):
+        origin.add_effect(HawksEyeEffect)
 
 class Barrage(Skill):
     name = "Barrage"
     animation_lock = SHORT_DELAY
     cooldown = 90
 
-    def use(self, player, target):
-        player.add_effect(BarrageEffect)
+    @staticmethod
+    def use(origin, target):
+        origin.add_effect(BarrageEffect)
 
 class XPotionOfDexterity(Skill):
     name = "X-Potion of Dexterity"
     animation_lock = LONG_DELAY
     cooldown = 270
 
-    def use(self, player, target):
-        player.add_effect(XPotionOfDexterityEffect)
+    @staticmethod
+    def use(origin, target):
+        origin.add_effect(XPotionOfDexterityEffect)
 
 class HeavyShot(Skill):
     name = "Heavy Shot"
     animation_lock = GCD_DELAY
     tp_cost = 60
 
-    def use(self, player, target):
+    @staticmethod
+    def use(origin, target):
         if random.random() < 0.2:
-            player.add_effect(StraighterShotEffect)
+            origin.add_effect(StraighterShotEffect)
+        target.add_damage(origin.calculate_damage(150)["potency"])
 
 class StraightShot(Skill):
     name = "Straight Shot"
     animation_lock = GCD_DELAY
     tp_cost = 70
 
-    def use(self, player, target):
-        if player.has_effect(StriaghterShotEffect):
-            player.remove_effect(StraighterShotEffect)
-        player.add_effect(StraightShotEffect)
+    @staticmethod
+    def use(origin, target):
+        if origin.has_effect(StriaghterShotEffect):
+            origin.remove_effect(StraighterShotEffect)
+            target.add_damage(origin.calculate_damage(140, { "guaranteed_critical": True })["potency"])
+        else:
+            target.add_damage(origin.calculate_damage(140)["potency"])
+        origin.add_effect(StraightShotEffect)
 
 class VenomousBite(Skill):
     name = "Venomous Bite"
     animation_lock = GCD_DELAY
     tp_cost = 80
 
-    def use(self, player, target):
-        target.add_effect(VenomousBiteEffect, player)
+    @staticmethod
+    def use(origin, target):
+        target.add_damage(origin.calculate_damage(100)["potency"])
+        target.add_effect(VenomousBiteEffect, origin)
 
 class Windbite(Skill):
     name = "Windbite"
     animation_lock = GCD_DELAY
     tp_cost = 80
 
-    def use(self, player, target):
-        target.add_effect(WindbiteEffect, player)
+    @staticmethod
+    def use(origin, target):
+        target.add_damage(origin.calculate_damage(60)["potency"])
+        target.add_effect(WindbiteEffect, origin)
 
 class FlamingArrow(Skill): 
     name = "Flaming Arrow"
     animation_lock = SHORT_DELAY
 
-    def use(self, player, target):
-        target.add_effect(FlamingArrowEffect, player)
+    @staticmethod
+    def use(origin, target):
+        target.add_effect(FlamingArrowEffect, origin)
 
 class BluntArrow(Skill):
     name = "Blunt Arrow"
     animation_lock = SHORT_DELAY
 
-    def use(self, player, target):
-        target.add_effect(SilenceEffect, player)
+    @staticmethod
+    def use(origin, target):
+        target.add_damage(origin.calculate_damage(50)["potency"])
+        target.add_effect(SilenceEffect, origin)
 
 class RepellingShot(Skill):
     name = "Repelling Shot"
     animation_lock = SHORT_DELAY
 
-    def use(self, player, target):
-        pass
+    @staticmethod
+    def use(origin, target):
+        target.add_damage(origin.calculate_damage(80)["potency"])
 
 class Bloodletter(Skill):
     name = "Bloodletter"
     animation_lock = SHORT_DELAY
 
-    def use(self, player, target):
-        pass
+    @staticmethod
+    def use(origin, target):
+        target.add_damage(origin.calculate_damage(150)["potency"])
 
 class Invigorate(Skill):
     name = "Invigorate"
     animation_lock = SHORT_DELAY
 
-    def use(self, player, target):
-        player.add_tp(400)
+    @staticmethod
+    def use(origin, target):
+        origin.add_tp(400)
 
 class EffectTimer:
-    def __init__(self):
-        self.duration = 0
-        self.snapshot = []`
+    def __init__(self, duration=0, snapshot=[]):
+        self.duration = duration
+        self.snapshot = snapshot
 
 class Actor:
     def __init__(self):
         self.effects = {}
 
+    def snapshot(self):
+        return effects.keys()
+
     # Adds an effect to the actor.
     def add_effect(self, effect, origin):
         key = (effect, origin)
-        self.effects[key] = effect.duration
+        effect_timer = None
+        if effect.has_snapshot:
+            effect_timer = EffectTimer(effect.duration, self.snapshot())
+        else:
+            effect_timer = EffectTimer(effect.duration)
+        self.effects[key] = effect_timer
 
     def remove_effect(self, effect, origin):
         key = (effect, origin)
@@ -222,6 +314,28 @@ class Player(Actor):
         self.job = job
         self.tp = 1000
 
+    # Does this belong here?
+    def calculate_damage(self, potency, **kwargs):
+        potency_modifier = 1.0
+        critical_hit_rate = 0.2
+
+        for effect_tuple in self.effects.keys():
+            cls = effect_tuple[0]
+            result = cls.damage_modifier()
+            if "potency_multiply" in result:
+                potency_modifier *= result["potency_multiply"]
+            if "critical_hit_rate_add" in result:
+                critical_hit_rate += result["critical_hit_rate_add"]
+
+        critical_hit = False
+        if random.random() < critical_hit_rate or kwargs.get("guaranteed_critical", False) is True:
+            critical_hit = True
+
+        return {
+            "potency": potency * potency_modifier * (1 + critical_hit * 0.5)
+            "critical_hit": critical_hit
+        }
+
     def add_tp(value):
         if value >= 0:
             self.tp = min(self.tp + value, 1000)
@@ -230,8 +344,21 @@ class Player(Actor):
 
 class Target(Actor):
     def __init__(self):
-        self.potency = 0
+        self.damage = 0 # Damage in potency... for now.
+
+    def calculate_dot_damage(self, potency, **kwargs):
+        pass
+
+    # TODO: should pass in origin since we need critical hit rates.
+    #       probably better to calculate damage in target since target debuffs like vulnerability up
+    #       may apply?
+    #       or calculate personal potency first and then pass it in to add_damage
+    #       for now let's do calcs elsewhere
+    def add_damage(self, potency):
+        self.damage += potency
 
 class Simulation:
     def __init__(self):
         pass
+
+print "Moo!"
