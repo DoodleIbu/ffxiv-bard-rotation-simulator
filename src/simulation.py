8113b2937ed5360fce8dc054d0a1cd518617261b
kernelthree <j.v.julien@gmail.com>
2015-03-23 21:59:17 -0400

    Windows support

--- src/simulation.py
+++ src/simulation.py
@@ -56,12 +56,14 @@ def worker(args):
 
     return total_damage
 
-count = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(processes=count)
-trials = 100000
-print sum(pool.map(worker, [{ "duration": 240, "trials": trials / count }] * count)) / trials
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    count = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=count)
+    trials = 100000
+    print sum(pool.map(worker, [{ "duration": 240, "trials": trials / count }] * count)) / trials
 
-# print worker({
-#     "duration": 240,
-#     "trials": 100
-# }) / 100
+    # print worker({
+    #     "duration": 240,
+    #     "trials": 100
+    # }) / 100
